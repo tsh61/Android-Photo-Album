2b2c66c3a303f3da94d85f6d381538eb70df836a
tsh81 <tsh61@scarletmail.rutgers.edu>
2020-12-09 21:02:26 -0500

    added opened album things

--- app/src/main/java/com/example/androidphotos27/MainActivity.java
+++ app/src/main/java/com/example/androidphotos27/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.androidphotos27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button add, delete, rename, open, search;
-    private ListView AlbumList;
+    private ListView albumList;
+    private Context context = this;
+    private int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,20 @@ public class MainActivity extends AppCompatActivity {
         rename = findViewById(R.id.rename);
         open = findViewById(R.id.open);
         search = findViewById(R.id.search);
+        albumList = findViewById(R.id.albList);
 
+    }
+
+    protected void onOpenClick(){
+        open.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent = new Intent(context, OpenedAlbum.class);
+               // startIntent.putExtra(ALBUM_ID, index);
+                startActivity(startIntent);
+            }
+
+        });
 
     }
 }
